7eec103e07292b2f4b6f352473a47e4c873b5f58
PatrikBuci <31506955+PatrikBuci@users.noreply.github.com>
2017-09-07 13:43:20 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,12 +1,28 @@
 ##these functions will create a special object that stores a numeric matrix and 
+##cache's its inverse matrix 
 
-## Write a short comment describing this function
 ##this is the function that creates a special matrix object that can cache an inverse
+##matrix from the input matrix (the input matrix has to be square and not singular of 
+##course)
+##the output of this function is a list of functions that set the value of a matrix,
+##get the value of a matrix, set the inverse matrix and get the inverse matrix
 
 makeCacheMatrix <- function(x = matrix()) {
+      slv <- NULL ##setting the inverse matrix to empty object as default
+      set <- function(y) {
+            x <<- y ##assign a value to an object in parent environment
+            slv <<- NULL
+      }
+      get <- function() x
+      setSolve <- function(solve) slv <<- solve
+      getSolve <- function() slv
+      list(set = set,
+           get = get,
+           setSolve = setSolve,
+           getSolve = getSolve)
 }
 
-## Write a short comment describing this function
+##the following function use as input the output of makeCacheMatrix (the special matrix
 ##object). The function first check if the inverse matrix have been already calculated
 #(through getSolve() function), if yes it returns the calculated inverse from cache, if
 #no it will calculate the inverse (through setSolve() function) by itself and return it 
